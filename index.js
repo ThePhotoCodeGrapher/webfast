3e6e41ab4765dc76fbd8b6f099a7a55972872088
Kai Gartner <kai@klikbait.nl>
2024-01-29 16:04:42 +0100

    Adding generator example for dynamic loading in module exports and depending on for example express.app

--- index.js
+++ index.js
@@ -15,6 +15,71 @@ async function set(program) {
 }
 
 // Program Fetch
+program.modules.dependOn = async function(reqFunc,program,name,callback) {
+    console.log(`Depend On Check`);
+    // Loop Through dependOn if its not true then check
+    for (let dependIndex in reqFunc.dependOn) {
+        const dependOn = reqFunc.dependOn[dependIndex];
+        // So we dependOn 
+        // Check index
+        // If .
+        let split = dependOn.split(`.`);
+        // Now we should loop through array
+        let objectData = program;
+        let fullObjectPath = `program`;
+        let dependOnItem = ``;
+        for (let spl in split) {
+            // Grab object
+            let toCheck = split[spl];
+            if (objectData[toCheck] == undefined) {
+                await setTimeout(async function(){
+                    return await program.modules.dependOn(reqFunc,program,name,callback);
+                },200);
+            } else {
+                // New object thing and og next
+                if (split.length-1 != spl) {
+                    fullObjectPath = fullObjectPath + `.${toCheck}`
+                } else {
+                    // Depend on
+                    dependOnItem = toCheck;
+                }
+                objectData = objectData[toCheck];
+            }
+        }
+
+        let progData = await eval(fullObjectPath);
+        const IndexCheck = Object.keys(progData).indexOf(dependOnItem);
+        if (IndexCheck == -1) {
+            // We need to wait and try again untill we can return
+            console.log(`DependOn Fail: ${dependOn}`);
+            await setTimeout(async function(){
+                return await program.modules.dependOn(reqFunc,program);
+            },200);
+        } else {
+            console.log(`DependOn Succes: ${dependOn}`);
+            progData[dependOnItem] = {
+                depend : dependOn,
+                state : true,
+                ts : Date.now()
+            }
+
+            // Now include this thing then
+            try {
+                program.modules[reqFunc.name] = await reqFunc.run(program,reqFunc.name);
+                if (callback != undefined) {
+                    return callback(program,name)
+                } else {
+                    return program;
+                }
+            } catch(err) {
+                // When erro
+                console.error(`Error Program add depend on require`);
+                console.error(err);
+            }
+        }
+    }
+}
+
 program.modules.fetch = async function(folder,program) {
     // TO Fetch folder modules
     program = await set(program);
@@ -25,6 +90,14 @@ program.modules.fetch = async function(folder,program) {
         // Check if folder or not
         await readDir.forEach(async (item) => {
             const itemPath = program.path.join(readPath, item);
+            // Get the filename with extension
+            const fileNameWithExtension = program.path.basename(itemPath);
+
+            // Get the filename without extension
+            const fileNameWithoutExtension = program.path.parse(fileNameWithExtension).name;
+
+            console.log('Filename without extension:', fileNameWithoutExtension);
+
             const isDirectory = program.fs.statSync(itemPath).isDirectory();
 
             if (!isDirectory) {
@@ -35,7 +108,24 @@ program.modules.fetch = async function(folder,program) {
             } else {
                 // It's directory so read the init file
                 const initPath = program.path.join(itemPath,`init.js`);
-                program = await (require(initPath)(program));
+
+                // Require first
+                const reqFunc = require(initPath);
+                const theType = typeof reqFunc;
+                switch (theType) {
+                    case `object`:
+                        // It's a object so check for dependend things etc.
+                        console.log(`Depending object`);
+                        await program.modules.dependOn(reqFunc,program,fileNameWithoutExtension,function(program,name){
+                            console.log(`Setup `,name)
+                        });
+                    break;
+                    case `function`:
+                        program = await (require(initPath)(program));
+                    break;
+                    default:
+                        console.error(`Error Missing typeOf item`);
+                }
             }
         });
 
